be46153757566f05c64e7130195cbd39077a5cc2
ChengHYuan <1467129814@qq.com>
2023-11-15 20:27:03 +0800

    第一次初始化

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ylur/users_file/CHY/ur_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ylur/users_file/CHY/ur_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/ylur/anaconda3/envs/ur_ws/bin:/home/ylur/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/ylur/users_file/CHY/ur_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/ylur/users_file/CHY/ur_ws/build'
+export ROS_PACKAGE_PATH="/home/ylur/users_file/CHY/ur_ws/src:$ROS_PACKAGE_PATH"
---
+++ build/ur10e_comunicate/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ylur/users_file/CHY/ur_ws/src/ur10e_comunicate
+# Build directory: /home/ylur/users_file/CHY/ur_ws/build/ur10e_comunicate
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
